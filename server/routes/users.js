03f8c5144f2990020a454944308c292ad5154c57
Aniket <aniketmund@gmail.com>
2023-03-26 16:42:15 +0530

    Completed the server part

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import {getUser,
-        getUserFriends,
-        addRemoveFriend,
-    } from '../controllers/users.js';
-import {verifyToken} from '../middleware/auth.js'
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-//create the read routes(CRUD ra R)
-//READ ROUTES
-router.get("/:id",verifyToken,getUser);
-router.get("/:id/friends",verifyToken,getUserFriends);
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
 
-//UPDATE
-router.patch("/:id/:friendId",verifyToken,addRemoveFriend)
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
